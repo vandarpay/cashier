9155b1a279adfcb017a18a3fbe5f1a6a153abc79
Mohammad Hossein Maazallahi <m.mazalahi@ef.net>
2021-08-28 14:04:39 +0430

    feat: add testing infrastructure

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Vandar\VandarCashier\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    // Set up any configuration for TestCase
+}
