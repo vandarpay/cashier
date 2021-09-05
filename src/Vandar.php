01a9804e4f9fcc173f201f95612d7abd1c6cc24a
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-09-05 22:57:44 +0430

    fix: add the ability not to append version to urls

--- src/Vandar.php
+++ src/Vandar.php
@@ -25,6 +25,7 @@ class Vandar
     public static function url(string $api, string $additional='', string $version_number=null): string
     {
         $api = strtoupper($api);
+        $append_version = true;
         switch ($api) {
             case 'IPG':
                 $base_url = config('vandar.ipg_base_url', 'https://ipg.vandar.io/');
@@ -35,12 +36,19 @@ class Vandar
                 break;
             case 'MANDATE':
                 $base_url = config('vandar.mandate_redirect_url', 'https://subscription.vandar.io/authorizations/');
+                $append_version = false;
                 break;
             case 'MANDATE_API':
-                $base_url = config('vandar.api_base_url', 'https://api.vandar.io/') . 'business/' . config('vandar.business_slug') . '/authorization/subscription';
+                $base_url = config('vandar.api_base_url', 'https://api.vandar.io/');
+                $additional = 'business/' . config('vandar.business_slug') . '/subscription/authorization/' . $additional;
+                $api = 'MANDATE';
                 break;
             default:
                 $base_url = config('vandar.api_base_url', 'https://api.vandar.io/');
+        }
+
+        if(! $append_version){
+            return $base_url . $additional;
         }
 
         return $base_url . 'v' . ($version_number ?? self::API_VERSIONS[$api]) . '/' . $additional;
