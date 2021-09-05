9f2f905deac5f087a4b0ea691002fcd547f51ead
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-09-05 21:36:23 +0430

    feat: implement SendMandateCreateRequest listener

--- src/Listeners/SendMandateCreateRequest.php
+++ src/Listeners/SendMandateCreateRequest.php
@@ -2,12 +2,31 @@
 
 namespace Vandar\Cashier\Listeners;
 
+use Illuminate\Validation\ValidationException;
+use Vandar\Cashier\Client\CasingFormatter;
+use Vandar\Cashier\Client\Client;
 use Vandar\Cashier\Events\MandateCreating;
+use Vandar\Cashier\Models\Mandate;
+use Vandar\Cashier\Vandar;
 
 class SendMandateCreateRequest
 {
-    public function handle(MandateCreating $mandate)
+    public function handle(MandateCreating $event)
     {
+        $payload = $event->mandate->only(['bank_code', 'mobile_number', 'count', 'limit', 'name', 'email', 'expiration_date', 'wage_type']);
+        $payload['callback_url'] = config('vandar.mandate_callback_url');
+        $payload['expiration_date'] = $payload['expiration_date'] ?? date('Y-m-d', strtotime(date('Y-m-d') . ' + 3 years'));
 
+        $payload = CasingFormatter::mobileKeyFormat($payload);
+
+        $response = Client::request('post', Vandar::url('MANDATE', 'business/' . config('vandar.business_slug') . '/authorization/subscription/store'), $payload, true);
+
+        if((! in_array($response->getStatusCode(), [200, 201])))
+        {
+            $event->mandate->status = Mandate::STATUS_FAILED;
+            throw ValidationException::withMessages($response->json()['errors']);
+
+        }
+        $event->mandate->token = $response->json()['token'];
     }
 }
