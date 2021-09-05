5723bdfe1481aaebe561d495e6b31c997fe85faf
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-09-05 22:17:26 +0430

    feat: enable tests for all three events that should be dispatched

--- src/Models/Withdrawal.php
+++ src/Models/Withdrawal.php
@@ -4,6 +4,7 @@ namespace Vandar\Cashier\Models;
 
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Model;
+use Vandar\Cashier\Events\WithdrawalCreating;
 
 class Withdrawal extends Model
 {
@@ -22,6 +23,10 @@ class Withdrawal extends Model
     const STATUS_DONE = 'DONE';
     const STATUS_CANCELED = 'CANCELED';
     const STATUS_FAILED = 'FAILED';
+
+    protected $dispatchesEvents = [
+        'creating' => WithdrawalCreating::class,
+    ];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
