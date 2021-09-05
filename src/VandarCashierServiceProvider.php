1205c1434197db72197b73a6b89d263e50697328
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-09-05 23:48:13 +0430

    docs: fix comments for configurations

--- src/VandarCashierServiceProvider.php
+++ src/VandarCashierServiceProvider.php
@@ -20,13 +20,13 @@ class VandarCashierServiceProvider extends ServiceProvider
         $config_path = realpath(__DIR__ . '/../config/vandar.php');
         
         
-        // php artisan vendor:publish --provider="Vandar\\VandarCashier\\VandarCashierServiceProvider" --tag=vandar-config
+        // php artisan vendor:publish --provider="Vandar\\Cashier\\VandarCashierServiceProvider" --tag=vandar-config
         $this->publishes([
             $config_path => config_path('vandar.php'),
         ], 'vandar-config');
         
         
-        // php artisan vendor:publish --provider="Vandar\\VandarCashier\\VandarCashierServiceProvider" --tag=vandar-migrations
+        // php artisan vendor:publish --provider="Vandar\\Cashier\\VandarCashierServiceProvider" --tag=vandar-migrations
         // $this->publishMigrations(Vandar::ACTIVE_MIGRATIONS);
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
