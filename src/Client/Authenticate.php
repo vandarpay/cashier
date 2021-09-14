c8518011619b0560284e6a88b5bb1e5013839b63
Amir <abouei.me@gmail.com>
2021-09-14 09:59:29 +0430

    fix: get refresh token from json file

--- src/Client/Authenticate.php
+++ src/Client/Authenticate.php
@@ -10,28 +10,27 @@ use Vandar\Cashier\Vandar;
 // TODO convert class to singleton
 class Authenticate
 {
-    public static function getToken() : string
+    public static function getToken(): string
     {
         // No previous token has been set, issue a completely new token.
-        if(is_null(Arr::get(self::getData(), 'access_token'))){
+        if (is_null(Arr::get(self::getData(), 'access_token'))) {
             self::setFreshToken();
         }
         // Token exists, but it has expired
-        else if(self::hasTokenExpired())
-        {
+        else if (self::hasTokenExpired()) {
             self::setRefreshedToken();
         }
 
         return Arr::get(self::getData(), 'access_token');
     }
 
-    protected static function setToken($array) : void
+    protected static function setToken($array): void
     {
         $array['expires_in'] += time();
         self::setData($array);
     }
 
-    protected static function setFreshToken() : void
+    protected static function setFreshToken(): void
     {
         $payload = ['mobile' => config('vandar.mobile'), 'password' => config('vandar.password')];
         $response = Client::request('post', Vandar::url('AUTH', 'login'), $payload, false)->json();
@@ -40,7 +39,7 @@ class Authenticate
 
     protected static function setRefreshedToken(): void
     {
-        $payload = ['refreshtoken' => config('vandar.auth.refresh_token')];
+        $payload = ['refreshtoken' => self::getData()['refresh_token']];
         $response = Client::request('post', Vandar::url('AUTH', 'refreshtoken'), $payload, false)->json();
         self::setToken($response);
     }
@@ -55,19 +54,18 @@ class Authenticate
         return time() >= Arr::get(self::getData(), 'expires_in', time() - 10);
     }
 
-    protected static function getData() : array
+    protected static function getData(): array
     {
         try {
             $data = json_decode(Storage::disk('local')->get('vandar_auth.json'), true);
-        } catch (FileNotFoundException $exception)
-        {
+        } catch (FileNotFoundException $exception) {
             $data = [];
         }
 
         return $data;
     }
 
-    protected static function setData(array $data) : void
+    protected static function setData(array $data): void
     {
         Storage::disk('local')->put('vandar_auth.json', json_encode($data));
     }
