eb98d8c00aa245e1781e8b1688ee0f7a5f5f1dc2
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-10-19 15:43:38 +0330

    fix: convert factor_number to factorNumber
    Fixes #12

--- src/Listeners/SendPaymentCreateRequest.php
+++ src/Listeners/SendPaymentCreateRequest.php
@@ -16,6 +16,8 @@ class SendPaymentCreateRequest
         $payload = $event->payment->only(['amount', 'mobile_number', 'factor_number', 'description', 'valid_card_number']);
         $payload['api_key'] = config('vandar.api_key');
         $payload['callback_url'] = config('vandar.callback_url');
+        $payload['factorNumber'] = $payload['factor_number'];
+        unset($payload['factor_number']);
 
         $payload = CasingFormatter::convertKeyFormat('camel', $payload, ['factor_number']);
 
