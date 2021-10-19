091ab9aa398554ddcd751449036c154f5b6ec27a
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-10-19 15:45:05 +0330

    fix: remove use of "route" function in config
    Fixes #10

--- src/Listeners/SendWithdrawalCreateRequest.php
+++ src/Listeners/SendWithdrawalCreateRequest.php
@@ -14,7 +14,7 @@ class SendWithdrawalCreateRequest
     public function handle(WithdrawalCreating $event)
     {
         $payload = $event->withdrawal->only(['authorization_id', 'amount', 'withdrawal_date', 'is_instant', 'max_retry_count', 'description']);
-        $payload['notify_url'] = config('vandar.notify_url');
+        $payload['notify_url'] = config('vandar.notify_url',  route('vandar.webhook.withdrawal'));
         $payload['withdrawal_date'] = $payload['withdrawal_date'] ?? date('Y-m-d');
 
         $response = Client::request('post', Vandar::url('WITHDRAWAL', 'store'), $payload, true);
