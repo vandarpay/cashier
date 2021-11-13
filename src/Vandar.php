53479b367309cb6653f3817a6035c43be8f911db
Mohammad Hossein Maazallahi <m.mazalahi@vandar.io>
2021-11-13 21:27:42 +0330

    docs: add other possible APIs to Vandar::url

--- src/Vandar.php
+++ src/Vandar.php
@@ -28,7 +28,7 @@ class Vandar
     }
 
     /**
-     * @param string $api can be AUTH, TRANSACTION, SETTLEMENT, IPG, MANDATE, or WITHDRAWAL
+     * @param string $api can be AUTH, TRANSACTION, SETTLEMENT, IPG, IPG_API, MANDATE, MANDATE_API, or WITHDRAWAL
      * @param string $additional Additional portion to add to the url
      * @param string|null $version_number the api version to which the requests should be sent, for backwards compatibility reasons
      * @return string full url for the selected endpoint
